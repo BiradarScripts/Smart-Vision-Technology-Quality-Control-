7f37072a8ae7dbf9e7a52814f914ee9d90981985
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-12-09 21:15:59 +0530

    Update

--- server/app.js
+++ server/app.js
@@ -1,8 +1,6 @@
-var createError = require('http-errors');
+// var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 const entryRoutes = require("./routes/data");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,15 +10,12 @@ const uploadItemsRoutes = require('./routes/upload');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', indexRouter);
@@ -30,20 +25,20 @@ app.use("/api", entryRoutes);
 app.use('/api', brandItemsRoute);
 app.use('/api', uploadItemsRoutes);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 const PORT = 3000;
 
